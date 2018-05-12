06fc160950f1675055e076b8724bae20617d1420
Diego Vizhnay <vizhnayd@gmail.com>
2018-05-12 12:09:34 -0400

    Created the GetRequest java file for our HTTP java code, testing it on the reports fragment for now | 5/12

--- app/src/main/java/edu/qc/cs370/macrotracker/fragments/ReportsFragment.java
+++ app/src/main/java/edu/qc/cs370/macrotracker/fragments/ReportsFragment.java
@@ -1,27 +1,25 @@
 package edu.qc.cs370.macrotracker.fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 import android.widget.TextView;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response.ErrorListener;
 import com.android.volley.Response.Listener;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import edu.qc.cs370.macrotracker.R;
-import org.json.JSONObject;
+import edu.qc.cs370.macrotracker.http.GetRequest;
 
 public class ReportsFragment extends Fragment {
-
   public ReportsFragment() {
     // Required constructor
   }
@@ -29,7 +27,23 @@ public class ReportsFragment extends Fragment {
   @Nullable
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.fragment_reports, parent, false);
+    final View view = inflater.inflate(R.layout.fragment_reports, parent, false);
+
+    // TODO try to figure out how to incorporate an onClick attribute for HTTP requests
+    // TODO instead of having to use onClickListeners (since they force you to declare
+    // TODO variables as final, etc.) - DV
+
+    Button requestBtn = view.findViewById(R.id.requestBtn);
+    final TextView testText = view.findViewById(R.id.testText);
+    final String url = "https://jsonplaceholder.typicode.com/users/1";
+
+    requestBtn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        // The below returns a string response, as opposed to a JSON object response, for testing purposes. - DV
+        GetRequest.testRequest(getContext(), testText, url);
+      }
+    });
 
     return view;
   }
@@ -37,22 +51,5 @@ public class ReportsFragment extends Fragment {
   @Override
   public void onViewCreated(View view, Bundle savedInstanceState) {
     // Setup any handles to view objects here
-    final TextView testingRequest = getView().findViewById(R.id.testText);
-    RequestQueue queue = Volley.newRequestQueue(getContext());
-    String url = "https://jsonplaceholder.typicode.com/users/1";
-
-    JsonObjectRequest jsonResponse = new JsonObjectRequest(Request.Method.GET, url, null,
-        new Listener<JSONObject>() {
-          @Override
-          public void onResponse(JSONObject response) {
-            testingRequest.setText("Response: " + response.toString());
-          }
-        }, new ErrorListener() {
-      @Override
-      public void onErrorResponse(VolleyError error) {
-        // Handle error
-      }
-    });
-
   }
 }
---
+++ app/src/main/java/edu/qc/cs370/macrotracker/http/GetRequest.java
@@ -0,0 +1,44 @@
+package edu.qc.cs370.macrotracker.http;
+
+import android.content.Context;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import android.view.View;
+import android.widget.TextView;
+
+public class GetRequest {
+  // Initialize the requestQueue, used for actually sending out the response.
+  private static RequestQueue requestQueue;
+  // Initalize the stringRequest, this will be changed to JSONRequest later on.
+  private static StringRequest stringRequest;
+  // Initializing a variable to set the incoming TextView to, from the calling activity.
+  private static TextView textView;
+
+  public static void testRequest(Context context, TextView text, String url) {
+    // Setting the RequestQueue variable to the context of the calling fragment.
+    requestQueue = Volley.newRequestQueue(context);
+    // Setting the TextView variable to the view of the calling fragment.
+    textView = text;
+    // The actual request to the API
+    stringRequest = new StringRequest(Request.Method.GET, url, new Listener<String>() {
+      @Override
+      public void onResponse(String response) {
+        // On successful reponse, set the TextView to the entire output of the response (in this case, JSON).
+        textView.setText(response.toString());
+      }
+    }, new ErrorListener() {
+      @Override
+      public void onErrorResponse(VolleyError error) {
+        textView.setText(error.toString());
+      }
+    });
+
+    // Add the StringRequest to the queue -- this is what actually makes the request! This is also asynchronous.
+    requestQueue.add(stringRequest);
+  }
+}
